a43dfa4bd516cfb39da22a442280fbc5432d0054
MANIMARAN-MSC <kirubamani2002@gmail.com>
2025-01-27 12:48:17 +0530

    Create DashbordScreen.dart

---
+++ lib/screens/DashbordScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+
+  final String title;
+
+  const DashboardScreen({super.key, required this.title});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
