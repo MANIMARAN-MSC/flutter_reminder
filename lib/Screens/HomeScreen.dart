3f53646c8fa9bb051a76e703ddc8c80e941fae76
MANIMARAN-MSC <kirubamani2002@gmail.com>
2025-01-29 15:40:03 +0530

    HomeScreen UI added

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reminder/screens/DashbordScreen.dart';
 import 'package:flutter_reminder/screens/SettingScreen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,6 +13,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final List<String> items = [
+    'Dashboard',
+    'Class Schedule Management',
+    'Event Remainder',
+    'Notifications',
+    'Calendar',
+    'Settings'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,8 +47,74 @@ class _HomeScreenState extends State<HomeScreen> {
 
         ],
       ) ,
-      body: Center(
-        child: Text(widget.title),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {
+
+                print(items[index]);
+                if (items[index] == 'Dashboard') {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const DashboardScreen(title: 'Dashboard'),
+                    ),
+                  );
+                } else if (items[index] == 'Messages') {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => const MessageScreen(title: 'Message'),
+                  //   ),
+                  // );
+                }
+                else if (items[index] == 'People') {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => const Peoplescreen(title: 'People'),
+                  //   ),
+                  // );
+                }
+                else if (items[index] == 'Battery') {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => const Batteryscreen(title: 'Battery'),
+                  //   ),
+                  // );
+                }
+                else if (items[index] == 'Settings') {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingScreen(title: 'Settings'),
+                    ),
+                  );
+                }
+
+
+              },
+              child: Card(
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Text(
+                    items[index],
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
