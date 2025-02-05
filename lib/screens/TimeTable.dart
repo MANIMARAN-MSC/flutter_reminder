b2de2991c3248f95178dc6a9e49ed38dfeb31b76
MANIMARAN-MSC <kirubamani2002@gmail.com>
2025-02-05 15:37:29 +0530

    Create TimeTable.dart

---
+++ lib/screens/TimeTable.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class TimeTable extends StatefulWidget {
+  const TimeTable({super.key});
+
+  @override
+  State<TimeTable> createState() => _TimeTableState();
+}
+
+class _TimeTableState extends State<TimeTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TimeTable"),
+      ),
+      body: Center(
+        child: Text("TimeTable"),
+      ),
+    );
+  }
+}
