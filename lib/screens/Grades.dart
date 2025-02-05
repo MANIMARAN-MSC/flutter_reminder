520f4185aaf6df7fd93196679c32168046157942
MANIMARAN-MSC <kirubamani2002@gmail.com>
2025-02-05 15:36:58 +0530

    Create Grades.dart

---
+++ lib/screens/Grades.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class Grades extends StatefulWidget {
+  const Grades({super.key});
+
+  @override
+  State<Grades> createState() => _GradesState();
+}
+
+class _GradesState extends State<Grades> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Grades"),
+      ),
+      body: Center(
+        child: Text("Grades"),
+      ),
+    );
+  }
+}
