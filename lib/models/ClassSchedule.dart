13c606e08009d6e002b1fc9801b8087f333c0303
MANIMARAN-MSC <kirubamani2002@gmail.com>
2025-02-05 14:22:37 +0530

    Create ClassSchedule.dart

---
+++ lib/models/ClassSchedule.dart
@@ -0,0 +1,8 @@
+class ClassSchedule {
+  final String subject;
+  final String teacher;
+  final String time;
+  final String day; // e.g., "Monday", "Tuesday"
+
+  ClassSchedule({required this.subject, required this.teacher, required this.time, required this.day});
+}
